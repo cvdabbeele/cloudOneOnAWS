8d638fd8b8a8a24a8d71bb19fc2100614269e4b9
cvdabbeele@gmail <cvdabbeele@gmail.com>
2021-02-05 18:30:45 +0100

    Fixed not uninstalling c1cs helm chart

--- down.sh
+++ down.sh
@@ -77,7 +77,7 @@ printf '%s\n' "C1CS: Removing from EKS cluster"
 helm_c1cs=`helm list -n c1cs -o json | jq -r '.[].name'`
 if [[ "${helm_c1cs}" == "trendmicro-c1cs" ]]; then
   printf "%s\n" "Unistalling C1CS"
-  helm delete trendmicro-c1c1 -n c1cs
+  helm delete trendmicro-c1cs -n c1cs
 fi
 
 #remove smartcheck 
