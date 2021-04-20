762538b5e50e40f8677cc2cf03b940acefd32956
cvdabbeele <cvdabbeele@gmail.com>
2021-04-20 22:01:17 +0200

    from main back to master

--- pushWithHighSecurityThresholds.sh
+++ pushWithHighSecurityThresholds.sh
@@ -2,8 +2,8 @@ STARTDIR=`pwd`
 cd ~/environment/apps/c1-app-sec-moneyx
 sed -i 's/": 0/": 300/g' buildspec.yml #change the security thresholds in the c1-app-sec-moneyx app
 echo " "  >>README.md  #ensure that we have a change, regardless if the above sed command made anychanges
-echo "pushing to main branch (20210303)"
-git add . && git commit -m "allowing risky builds"   && git push  --set-upstream origin main
+echo "pushing to master branch (20210303)"
+git add . && git commit -m "allowing risky builds"   && git push  --set-upstream origin master
 
 cd $STARTDIR
 
