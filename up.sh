9bd04f4d2e7e9073718054c47f5b1b7f1fb55f96
cvdabbeele <cvdabbeele@gmail.com>
2021-09-16 09:37:21 +0200

    wip

--- up.sh
+++ up.sh
@@ -28,6 +28,8 @@
 PROJECTDIR=`pwd` 
 export ACCOUNT_ID=`aws sts get-caller-identity | jq -r '.Account'`
 export AWS_REGION=`aws configure get region`
+export AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id`
+export AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key`
 
 varsok=true
 # Check AWS settings
