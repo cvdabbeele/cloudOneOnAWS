28c509a54aa0a58c19f76b8da8f4fc16090629ec
cvdabbeele <cvdabbeele@gmail.com>
2021-10-07 11:09:02 +0200

    API URL for C1CS without keyword "api"

--- C1AS.sh
+++ C1AS.sh
@@ -7,8 +7,13 @@ function create_c1as_group {
 # parm = ${1} = name of the app"
 # Creating groups
 # if a group object for this project-app already exists in c1as, then delete it first
+
+
 TEMPJSON=(`\
-curl --silent --location --request GET "${C1ASAPIURL}/accounts/groups" --header 'Content-Type: application/json' --header "${C1AUTHHEADER}" --header 'api-version: v1' \
+curl --silent --location --request GET "${C1ASAPIURL}/accounts/groups" \
+--header 'Content-Type: application/json' \
+--header "${C1AUTHHEADER}" \
+--header 'api-version: v1' \
 `)
 C1ASGROUPS=(`echo "$TEMPJSON" | jq   -r ".[].name"`)
 C1ASGROUPIDS=(`echo "$TEMPJSON" | jq   -r ".[].group_id"`)
